11293acdb1e6a347c4f8cbf37ef128d31b0d67d7
Dishakamble19 <dishakamble19@gmail.com>
2023-01-05 14:01:36 +0530

    message

---
+++ src/main/java/com/jsp/demo/Pen.java
@@ -0,0 +1,19 @@
+package com.jsp.demo;
+
+public class Pen {
+	
+	
+	Pen(){
+		
+		
+		System.out.println("constructed loading");
+		
+	}
+	 
+	public void write() {
+		
+		System.out.println("Pen is writing");
+		
+	}
+	}
+
